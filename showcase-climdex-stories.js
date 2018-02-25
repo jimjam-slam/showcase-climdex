1493843a04b1a9b5e94cb4eb80fb912295c7c8bd
James Goldie (Rensa) <me@rensa.co>
2018-02-25 15:56:48 +1100

    Story JS file stub

---
+++ showcase-climdex-stories.js
@@ -0,0 +1,21 @@
+/* blah */
+
+var app_mode = 'data';    // or 'tour'
+
+var stories = [
+  {
+    name_short: 'seusa_warming_hole',
+    name_long: 'South-east US warming hole',
+    index: 'CDD_ann',
+    zoom: 3,
+    center_start: [-10, -80],
+    center_end: [-10, -80],
+    timeDimensionOptions: {
+      timeInterval: '1995-01-01T00:00:00.000Z/2008-01-01T00:00:00.000Z',
+      period: 'P1Y'
+    },
+  },
+  {
+
+  }
+]

4c9c754b121d99957a43143354ef7727b03c9f33
James Goldie (Rensa) <me@rensa.co>
2018-02-26 20:20:06 +1100

    Reorganise layer tree (warning: think it needs more work)

--- layertree.js
+++ layertree.js
@@ -1,41 +1,73 @@
 /* set of layers with which to populate my custom controls */
 
-var layerMenuSet = {
+var baseLayerSet = {
   layers: {
-    
+    'CDD_ann_series': L.timeDimension.layer.wms(
+      L.tileLayer.wms(
+        geoserver_base + '/climdex/wms?',
+        $.extend({}, geoserver_options, { layers: 'CDD_ann_series' })),
+      { cache: 12 }),
+    'CSDI_ann_series': L.timeDimension.layer.wms(
+      L.tileLayer.wms(
+        geoserver_base + '/climdex/wms?',
+        $.extend({}, geoserver_options, { layers: 'CSDI_ann_series' })),
+      { cache: 12 }),
+    'TXx_ann_series': L.timeDimension.layer.wms(
+      L.tileLayer.wms(
+        geoserver_base + '/climdex/wms?',
+        $.extend({}, geoserver_options, { layers: 'TXx_ann_series' })),
+      { cache: 12 }),
+    'TXx_jan_series': L.timeDimension.layer.wms(
+      L.tileLayer.wms(
+        geoserver_base + '/climdex/wms?',
+        $.extend({}, geoserver_options, { layers: 'TXx_jan_series' })),
+      { cache: 12 }),
+    'TXx_jul_series': L.timeDimension.layer.wms(
+      L.tileLayer.wms(
+        geoserver_base + '/climdex/wms?',
+        $.extend({}, geoserver_options, { layers: 'TXx_jul_series' })),
+      { cache: 12 })
   },
 
   controls: {
-    indexDropdown: {
+    primaryDropdown: {
       CDD: {
-        index: 'CDD',
-        name: 'Maximum length of dry spell',
-        description: 'Maximum number of consecutive days with RR < 1mm',
+        name: 'CDD',
+        description: 'Maximum length of dry spell: maximum number of consecutive days with RR < 1mm',
         controlType: 'css_dropdown',
-        options: {
+        other_options: {
           seasonDropdown: ['ann'],
-          typeDropdown: ['vals', 'trend', 'avg']
+          typeDropdown: ['series']
         }
       },
       CSDI: {
-        index: 'CSDI',
-        name: 'Cold spell duration index',
-        description: 'Annual count of days with at least 6 consecutive days when TN < 10th percentile',
+        name: 'CSDI',
+        description: 'Cold spell duration index: annual count of days with at least 6 consecutive days when TN < 10th percentile',
         controlType: 'css_dropdown',
-        options: {
+        other_options: {
           seasonDropdown: ['ann'],
-          typeDropdown: ['vals', 'trend', 'avg']
+          typeDropdown: ['series']
         }
       },
-
-
+      TXx: {
+        name: 'TXx',
+        description: 'Monthly maximum value of daily maximum temperature',
+        controlType: 'css_dropdown',
+        other_options: {
+          seasonDropdown: ['ann', 'jan', 'jul'],
+          typeDropdown: ['series']
+        }
+      }
     },
-    seasonDropdown: {
-  
-    },
-    typeDropdown: {
-  
+    others: {
+      seasonDropdown: {
+        ann: 'Year',
+        jan: 'January',
+        jul: 'July'
+      },
+      typeDropdown: {
+        series: 'Series'
+      }
     }
   }
-
 }
